2dc43b621a4b424458c5ad3379553b3dda736495
Tuan Nguyen <ravennguyen113@gmail.com>
2024-07-01 23:30:31 +0700

    ok

---
+++ src/main/java/com/tuanbook/spring_boot_library/config/MyDataRestConfig.java
@@ -0,0 +1,35 @@
+package com.tuanbook.spring_boot_library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.tuanbook.spring_boot_library.entity.Book;
+import com.tuanbook.spring_boot_library.entity.Review;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = { HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.PATCH };
+        config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
+
+        /* Configure CORS Mapping */
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config,
+            HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metdata, httpMethod) -> httpMethod.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethod) -> httpMethod.disable(theUnsupportedActions));
+    }
+}
